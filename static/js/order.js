e891fe6395a3cddd71da479fb6c78dcd2b4f9336
Augusto Neftalí Ruiz Cauich <mgr.ruiz.56@gmail.com>
2018-07-24 11:52:30 -0500

    estilos y js incluidos

--- static/js/order.js
+++ static/js/order.js
@@ -1,11 +1,32 @@
-var items = document.getElementsByClassName("order-item");
-items = [].slice.call(items) 
-console.log(items)
-items.sort( (a,b)=>{ new Date(b.getAttribute("data-date")) - new Date(a.getAttribute("data-date")) } )
-console.log(items)
+var posts_list = document.getElementById("posts-list");
 
-var list = document.getElementsByClassName("order-menu")[0];
-list.innerHTML="";
-items.forEach(element => {
-    list.appendChild(element)
+fetch('/static/js/blog/postsDB.json')
+.then(function(response) {
+  return response.json();
+})
+.then(function(db) {
+  fillPostList(db);
 });
+
+function fillPostList( data ){
+    data.forEach(element => {
+        var li = document.createElement("li");
+        
+        li.innerHTML  = "<a href=\""+ element.url+"\">" + element.date  + " - " + element.title + "</a>";
+        posts_list.appendChild(li);
+    });
+    
+}
+
+
+// var items = document.getElementsByClassName("order-item");
+// items = [].slice.call(items) 
+// console.log(items)
+// items.sort( (a,b)=>{ new Date(b.getAttribute("data-date")) - new Date(a.getAttribute("data-date")) } )
+// console.log(items)
+
+// var list = document.getElementsByClassName("order-menu")[0];
+// list.innerHTML="";
+// items.forEach(element => {
+//     list.appendChild(element)
+// });
